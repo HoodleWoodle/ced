d08a1a630f458f1bb369d7901ed4189c73d4c046
hoodlewoodle@web.de <hoodlewoodle@web.de>
2025-03-11 10:53:46 +0100

    add left section for line number

--- src/ui.c
+++ src/ui.c
@@ -19,7 +19,7 @@ namespace ced::ui {
         float top;
         float right;
         float bottom;
-    } margin_t;
+    } rect_offset_t;
 
     typedef struct {
         color_t color;
@@ -31,18 +31,25 @@ namespace ced::ui {
     } cfg_frame_t;
 
     typedef struct {
+        cfg_bg_t bg;
+        cfg_frame_t frame;
+        rect_offset_t padding;
+    } cfg_section_t;
+
+    typedef struct {
         font_t* font;
         color_t color;
-        margin_t margin;
-        float padding;
+        float line_spacing;
 
         u64 tab_space_count;
     } cfg_text_t;
 
     typedef struct {
-        cfg_bg_t bg;
-        cfg_frame_t frame;
+        cfg_section_t main;
+        cfg_section_t left;
         cfg_text_t text;
+
+        bool left_is_enabled;
     } cfg_panel_t;
 
     // ##################################################################
@@ -57,7 +64,8 @@ namespace ced::ui {
     // constants
     // ##################################################################
 
-    static const char* FONT_VISIBLE_CHARACTERS = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+    static const char* FONT_CHARACTERS_VISIBLE = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+    static const char* FONT_CHARACTERS_NUM = "0123456789";
 
     // ##################################################################
     // globals
@@ -112,29 +120,81 @@ namespace ced::ui {
 		size2_t size = dze_window_framebuffer_size();
         const cfg_panel_t* cfg = &s_cfg_panel_default;
 
-        qtransform_t t = {0}; t.p = pos; t.s = size;
-        dze_qrenderer_draw_quad(t, cfg->bg.color);
-        dze_qrenderer_draw_frame(t, cfg->frame.thickness, cfg->frame.color);
+        char buf[1024];
 
-        float font_size_y = dze_font_sizeof(cfg->text.font, FONT_VISIBLE_CHARACTERS).y;
-        u64 draw_line_max = (t.s.y - 2 * cfg->frame.thickness) / (font_size_y + cfg->text.padding);
+        float font_size_y = dze_font_sizeof(cfg->text.font, FONT_CHARACTERS_VISIBLE).y;
+        float font_num_size_x_max = 1.0f;
+        buf[1] = '\0';
+        for (const char* c = FONT_CHARACTERS_NUM; *c != '\0'; c++) {
+            buf[0] = *c;
+            float num_size_x = dze_font_sizeof(cfg->text.font, buf).x;
+            if (num_size_x > font_num_size_x_max)
+                font_num_size_x_max = num_size_x;
+        }
+
+        float tm_size_y = size.y;
+        u64 draw_line_num_max = (tm_size_y - 2 * cfg->main.frame.thickness) / (font_size_y + cfg->text.line_spacing);
+        u64 line_num = 1;
+        u64 line_num_max = line_num + draw_line_num_max - 1;
+
+        u64 line_num_max_digits_max = 1;
+        if (line_num_max >= 100000000) { line_num_max_digits_max += 8; line_num_max /= 100000000; }
+        if (line_num_max >= 10000) { line_num_max_digits_max += 4; line_num_max /= 10000; }
+        if (line_num_max >= 100) { line_num_max_digits_max += 2; line_num_max /= 100; }
+        if (line_num_max >= 10) { line_num_max_digits_max += 1; }
+        
+        // left
+        qtransform_t tl = {0};
+        if (cfg->left_is_enabled) {
+            tl.p = pos;
+            tl.s = (size2_t){
+                2 * cfg->left.frame.thickness + cfg->left.padding.left + cfg->left.padding.right + line_num_max_digits_max * font_num_size_x_max,
+                size.y
+            };
+            dze_qrenderer_draw_quad(tl, cfg->left.bg.color);
+            dze_qrenderer_draw_frame(tl, cfg->left.frame.thickness, cfg->left.frame.color);
+        }
+
+        // main
+        qtransform_t tm = {0};
+        tm.p = (pos2_t){ tl.p.x + tl.s.x, pos.y };
+        tm.s = (size2_t){ size.x - tl.s.x, tm_size_y };
+        dze_qrenderer_draw_quad(tm, cfg->main.bg.color);
+        dze_qrenderer_draw_frame(tm, cfg->main.frame.thickness, cfg->main.frame.color);
+
+        // text
         u64 draw_line_idx = 0;
-
         doc::slice_t slice = {0};
         const char* slice_progress = NULL;
-
-        char buf[1024];
         while (get_next_draw_line(it, &slice, &slice_progress, buf, sizeof(buf), cfg)) {
-            qtransform_t tt = {0};
-            tt.p = (pos2_t){
-                t.p.x + cfg->frame.thickness + cfg->text.margin.left,
-                t.p.y + cfg->frame.thickness + cfg->text.margin.top + draw_line_idx * (cfg->text.padding + font_size_y)
-            };
-            dze_qrenderer_draw_text(cfg->text.font, tt.p, tt.r, buf, cfg->text.color);
+            if (cfg->left_is_enabled) {
+                // text - left
+                char line_num_buf[21];
+                sprintf(line_num_buf, "%lu", line_num);
+                float line_num_buf_size_x = dze_font_sizeof(cfg->text.font, line_num_buf).x;
 
+                qtransform_t ttl = {0};
+                ttl.p = (pos2_t){
+                    tl.p.x + tl.s.x - cfg->left.frame.thickness - cfg->left.padding.right - line_num_buf_size_x,
+                    tl.p.y + cfg->left.frame.thickness + cfg->left.padding.top + draw_line_idx * (cfg->text.line_spacing + font_size_y)
+                };
+                dze_qrenderer_draw_text(cfg->text.font, ttl.p, ttl.r, line_num_buf, cfg->text.color);
+            }
+
+            // text - main
+            qtransform_t ttm = {0};
+            ttm.p = (pos2_t){
+                tm.p.x + cfg->main.frame.thickness + cfg->main.padding.left,
+                tm.p.y + cfg->main.frame.thickness + cfg->main.padding.top + draw_line_idx * (cfg->text.line_spacing + font_size_y)
+            };
+            dze_qrenderer_draw_text(cfg->text.font, ttm.p, ttm.r, buf, cfg->text.color);
+
+            line_num++;
             draw_line_idx++;
-            if (draw_line_idx >= draw_line_max)
+            if (draw_line_idx >= draw_line_num_max)
                 break;
+
+            // TODO: IMPROVE: line number of last trailing newline is not shown
         }
 
         // TODO: IMPL: scrolling
@@ -145,15 +205,22 @@ namespace ced::ui {
     // ##################################################################
     
     static void cfg_panel_init_default(cfg_panel_t* cfg) {
-        cfg->bg.color = COLOR_U8(21, 25, 35);
+        *cfg = {0};
+
+        cfg->main.bg.color = COLOR_U8(21, 25, 35);
+        cfg->main.frame.color = COLOR_U8(180, 180, 180);
+        cfg->main.frame.thickness = 1;
+        cfg->main.padding = (rect_offset_t){ 10.0f, 5.0f, 5.0f, 5.0f };
+
+        cfg->left.bg.color = COLOR_U8(6, 10, 20);
+        cfg->left.padding = (rect_offset_t){ 10.0f, 5.0f, 5.0f, 5.0f };
 
         cfg->text.font = &s_font_default;
         cfg->text.color = COLOR_WHITE;
-        cfg->text.margin = (margin_t){ 5.0f, 5.0f, 5.0f, 5.0f };
-        cfg->text.padding = 5.0f;
+        cfg->text.line_spacing = 5.0f;
         cfg->text.tab_space_count = 4;
 
-        //COLOR_U8(6, 10, 20)
+        cfg->left_is_enabled = true;
     }
 
     static bool get_next_draw_line(doc::iter_t* it, doc::slice_t* slice, const char** slice_progress, char* buf, u64 buf_len, const cfg_panel_t* cfg) {
@@ -170,7 +237,7 @@ namespace ced::ui {
                     if (pwrite == buf) {
                         return false;
                     }
-                    break;
+                    goto ret; // yay !! goto !!
                 }
                 *slice_progress = slice->buf;
             }
